696e2ace1438220bdb4d7534fcb17f648ee3d6b1
nahomynvh <nahomy.nvh10@aluno.ifsc.edu.br>
2024-10-07 20:57:35 +0000

    criaçao de sala, imagem de abertura, centralização do jogo.

--- adc/index.js
+++ adc/index.js
@@ -1,8 +1,13 @@
 import config from './config.js'
-class Game extends Phaser.game {
-    consructor () {
-        super(config)
-    }
+import abertura from './abertura.js'
+import sala from './sala.js'
+
+class Game extends Phaser.Game {
+    constructor () {
+			super(config)
+			this.scene.add('abertura', abertura)
+			this.scene.start ('abertura')
+		}
 } 
 
 window.onload = () => {
